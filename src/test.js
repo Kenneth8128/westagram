b75fdd5d0ad7e4b5de181425132879456748b81d
kenneth8128 <kenkim@younggap.co>
2020-05-20 22:44:13 +0900

    first one

---
+++ src/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export class test extends React.Component {
+  render() {
+    return (
+      <div><h1>
+          가나다라
+          </h1></div>
+    )
+  }
+}
+
+export default test;
